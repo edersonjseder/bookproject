f4b988e942bb0e5595c7553e56cd4ef4f460e384
Ederson de Jesus <edersonjseder@gmail.com>
2022-03-14 17:12:02 +0100

    [book_project_release_1.0] Added more features to spring boot and angular frontend files

---
+++ book/src/main/java/com/ecommerce/book/entity/Product.java
@@ -0,0 +1,43 @@
+package com.ecommerce.book.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+    @Column(name = "image_url")
+    private String imageUrl;
+    private boolean active;
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+    @Column(name = "date_created")
+    private Date dateCreated;
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+}
